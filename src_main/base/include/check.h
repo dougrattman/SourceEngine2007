25d6e72aec87aea4377400c29ce2930d0421a6ae
Doug Rattman <d.rattman@aol.com>
2018-04-17 21:21:59 +0300

    [base] Use std::exit to normally terminate CRT in CHECK/DCHECK macro

--- src_main/base/include/check.h
+++ src_main/base/include/check.h
@@ -13,20 +13,25 @@
 #define SOURCE_DBG_CODE_NOSCOPE(code_) ((void)0)
 #endif  // !NDEBUG
 
+// Checks |condition| in DEBUG mode, if not, then print condition and
+// |exit_code| to stderr and exit process with |exit_code|. Process is
+// terminated via std::exit call.
 #define DCHECK(condition, exit_code)                   \
   assert(condition);                                   \
   SOURCE_DBG_CODE_NOSCOPE(if (!(condition)) {          \
     fprintf_s(stderr, "%s failed (%d).", (#condition), \
               static_cast<int>(exit_code));            \
-    std::_Exit(exit_code);                             \
+    std::exit(exit_code);                              \
   })
 
+// Checks |condition|, if not, then print condition and |exit_code| to stderr
+// and exit process with |exit_code|. Process is terminated via std::exit call.
 #define CHECK(condition, exit_code)                    \
   assert(condition);                                   \
   if (!(condition)) {                                  \
     fprintf_s(stderr, "%s failed (%d).", (#condition), \
               static_cast<int>(exit_code));            \
-    std::_Exit(exit_code);                             \
+    std::exit(exit_code);                              \
   }
 
 #endif  // BASE_INCLUDE_CHECK_H_
