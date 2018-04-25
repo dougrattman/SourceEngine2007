756c09cd68c7ed6e61106523547cf88ef6737edf
Doug Rattman <d.rattman@aol.com>
2018-04-25 15:59:43 +0300

    [engine] Add missed mathlib init header

---
+++ src_main/engine/initmathlib.h
@@ -0,0 +1,8 @@
+// Copyright © 1996-2018, Valve Corporation, All rights reserved.
+
+#ifndef SOURCE_ENGINE_INITMATHLIB_H_
+#define SOURCE_ENGINE_INITMATHLIB_H_
+
+void InitMathlib();
+
+#endif  // !SOURCE_ENGINE_INITMATHLIB_H_
