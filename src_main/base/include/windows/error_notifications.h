8a98695a3b717af4813769c67b9ea0d85b925246
Doug Rattman <d.rattman@aol.com>
2018-05-06 21:50:36 +0300

    [windows] Unicode/ANSI notifications support

--- src_main/base/include/windows/error_notifications.h
+++ src_main/base/include/windows/error_notifications.h
@@ -3,33 +3,40 @@
 #ifndef BASE_INCLUDE_WINDOWS_ERROR_NOTIFICATIONS_H_
 #define BASE_INCLUDE_WINDOWS_ERROR_NOTIFICATIONS_H_
 
+#include <tchar.h>
 #include "base/include/base_types.h"
 #include "base/include/windows/windows_errno_info.h"
 #include "base/include/windows/windows_light.h"
 
+#ifdef _UNICODE
+#define tstr wstr
+#else
+#define tstr str
+#endif
+
 namespace source::windows {
 // Build error from message |message| and error info |errno_info|.
-inline wstr BuildError(_In_ wstr message,
+inline tstr BuildError(_In_ tstr message,
                        _In_ const windows_errno_info& errno_info) {
-  message += L"\n\nPrecise error description: ";
+  message += TEXT("\n\nPrecise error description: ");
   message += errno_info.description;
   return message;
 }
 
 // Build error from message |message| and error code |errno_code|.
-inline wstr BuildError(_In_ wstr message, _In_ windows_errno_code errno_code) {
+inline tstr BuildError(_In_ tstr message, _In_ windows_errno_code errno_code) {
   return BuildError(message, make_windows_errno_info(errno_code));
 }
 
 // Show error box with |message|.
-inline void ShowErrorBox(_In_ wstr message) {
-  MessageBoxW(nullptr, message.c_str(), SOURCE_APP_NAME L" - Error",
-              MB_ICONERROR | MB_OK);
+inline void ShowErrorBox(_In_ tstr message) {
+  MessageBox(nullptr, message.c_str(), SOURCE_APP_NAME TEXT(" - Error"),
+             MB_ICONERROR | MB_OK);
 }
 
 // Show error box with |message| by |errno_info|.
 inline windows_errno_code NotifyAboutError(
-    _In_ wstr message,
+    _In_ tstr message,
     _In_ windows_errno_info errno_info = windows_errno_info_last_error()) {
   ShowErrorBox(BuildError(message, errno_info));
   return errno_info.code;
@@ -37,7 +44,7 @@ inline windows_errno_code NotifyAboutError(
 
 // Show error box with |message| by |errno_code|.
 inline windows_errno_code NotifyAboutError(
-    _In_ wstr message,
+    _In_ tstr message,
     _In_ windows_errno_code errno_code = windows_errno_code_last_error()) {
   return NotifyAboutError(message, make_windows_errno_info(errno_code));
 }
