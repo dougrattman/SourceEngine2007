f8ab3cd85fbd2e9b28b501dbc6894b8103c75544
Doug Rattman <d.rattman@aol.com>
2018-05-20 22:53:20 +0300

    [build] Remove unused PNACL arch

--- src_main/build/include/build_config.h
+++ src_main/build/include/build_config.h
@@ -85,8 +85,6 @@
 #define ARCH_CPU_ARMEL 1
 #define ARCH_CPU_32_BITS 1
 #define ARCH_CPU_LITTLE_ENDIAN 1
-#elif defined(__pnacl__)
-#define ARCH_CPU_32_BITS 1
 #elif defined(__MIPSEL__)
 #define ARCH_CPU_MIPS_FAMILY 1
 #define ARCH_CPU_MIPSEL 1
