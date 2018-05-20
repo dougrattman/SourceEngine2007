00e4cca29b0fb72a5c99cc9366809f5711709132
Doug Rattman <d.rattman@aol.com>
2018-05-20 23:30:43 +0300

    [launcher] Check CPU supported. better error messages

--- src_main/launcher/launcher_main_windows.cc
+++ src_main/launcher/launcher_main_windows.cc
@@ -7,13 +7,12 @@
 
 #include "build/include/build_config.h"
 
-#ifdef OS_WIN
+#include "base/include/windows/error_notifications.h"
 #include "base/include/windows/scoped_winsock_initializer.h"
 #include "base/include/windows/windows_light.h"
 
 #include <shellapi.h>
 #include <shlwapi.h>  // registry stuff
-#endif
 
 #include "appframework/AppFramework.h"
 #include "base/include/base_types.h"
@@ -21,6 +20,7 @@
 #include "engine_launcher_api.h"
 #include "tier0/include/dbg.h"
 #include "tier0/include/icommandline.h"
+#include "tier0/include/system_info.h"
 #include "tier0/include/vcrmode.h"
 #include "tier1/strtools.h"
 
@@ -33,10 +33,11 @@
 #include "tier0/include/memdbgon.h"
 
 namespace {
-// Spew function!
+// Spew function.
 SpewRetval_t LauncherSpewFunc(_In_ SpewType_t spew_type,
                               _In_z_ ch const *message) {
   OutputDebugStringA(message);
+
   switch (spew_type) {
     case SPEW_MESSAGE:
     case SPEW_LOG:
@@ -84,7 +85,7 @@ const ch *ComputeBaseDirectoryFromCommandLine(
   return nullptr;
 }
 
-// Gets the executable name
+// Gets the executable name.
 inline source::windows::windows_errno_code GetExecutableName(
     _In_ ch *exe_name, usize exe_name_length) {
   const HMODULE exe_module = GetModuleHandleW(nullptr);
@@ -100,10 +101,10 @@ inline source::windows::windows_errno_code GetExecutableName(
 std::tuple<const ch *, source::windows::windows_errno_code>
 ComputeBaseDirectoryFromExePath() {
   static ch base_directory[SOURCE_MAX_PATH];
-  const source::windows::windows_errno_code return_code{
+  const source::windows::windows_errno_code errno_code{
       GetExecutableName(base_directory, std::size(base_directory))};
 
-  if (source::windows::succeeded(return_code)) {
+  if (source::windows::succeeded(errno_code)) {
     ch *last_backward_slash{strrchr(base_directory, '\\')};
     if (*last_backward_slash) {
       *(last_backward_slash + 1) = '\0';
@@ -121,7 +122,7 @@ ComputeBaseDirectoryFromExePath() {
     Q_FixSlashes(base_directory);
   }
 
-  return {base_directory, return_code};
+  return {base_directory, errno_code};
 }
 
 // Purpose: Determine the directory where this .exe is running from
@@ -168,22 +169,22 @@ std::tuple<bool, source::windows::windows_errno_code> InitTextModeIfNeeded(
     return {false, S_OK};
   }
 
-  source::windows::windows_errno_code return_code{
+  source::windows::windows_errno_code errno_code{
       AllocConsole() != FALSE
           ? S_OK
           : source::windows::windows_errno_code_last_error()};
-  if (source::windows::succeeded(return_code)) {
+  if (source::windows::succeeded(errno_code)) {
     FILE *dummy;
     // reopen stdin, stout, stderr handles as console window input, output and
     // output.
-    return_code = !freopen_s(&dummy, "CONIN$", "rb", stdin) &&
-                          !freopen_s(&dummy, "CONOUT$", "wb", stdout) &&
-                          !freopen_s(&dummy, "CONOUT$", "wb", stderr)
-                      ? S_OK
-                      : E_FAIL;  // Yea, no direct mapping errno => HRESULT :(
+    errno_code = !freopen_s(&dummy, "CONIN$", "rb", stdin) &&
+                         !freopen_s(&dummy, "CONOUT$", "wb", stdout) &&
+                         !freopen_s(&dummy, "CONOUT$", "wb", stderr)
+                     ? S_OK
+                     : E_FAIL;  // Yea, no direct mapping errno => HRESULT :(
 
     // freopen set last error in errno (not 0 => not NO_ERROR).
-    if (return_code != S_OK) {
+    if (errno_code != S_OK) {
       ch error_description[96];
       strerror_s(error_description, errno);
       Error(
@@ -193,13 +194,13 @@ std::tuple<bool, source::windows::windows_errno_code> InitTextModeIfNeeded(
     }
   }
 
-  if (return_code == S_OK) {
-    return_code = SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE) != FALSE
-                      ? S_OK
-                      : source::windows::windows_errno_code_last_error();
+  if (errno_code == S_OK) {
+    errno_code = SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE) != FALSE
+                     ? S_OK
+                     : source::windows::windows_errno_code_last_error();
   }
 
-  return {source::windows::succeeded(return_code), return_code};
+  return {source::windows::succeeded(errno_code), errno_code};
 }
 
 // Remove all but the last -game parameter. This is for mods based off something
@@ -229,7 +230,7 @@ void RemoveSpuriousGameParameters(ICommandLine *const command_line) {
   }
 }
 
-void RelaunchWithNewLanguageViaSteam() {
+void RelaunchIfNeeded() {
   // If there is a URL here, exec it. This supports the capability of
   // immediately re-launching the game via Steam in a different audio language.
   HKEY source_key;
@@ -251,36 +252,37 @@ void RelaunchWithNewLanguageViaSteam() {
                                   // 'HINSTANCE' to 'int'
       // 'reinterpret_cast': truncation from 'HINSTANCE' to 'int'
       MSVC_DISABLE_WARNING(4311)
-      u32 return_code = static_cast<u32>(reinterpret_cast<i32>(ShellExecuteA(
+      u32 errno_code = static_cast<u32>(reinterpret_cast<i32>(ShellExecuteA(
           nullptr, "open", relaunch_url, nullptr, nullptr, SW_SHOW)));
       MSVC_END_WARNING_OVERRIDE_SCOPE();
 
-      // And 0 <=> The operating system is out of memory or resources.
-      return_code =
-          return_code > maxErrorCode
+      // And 0 means the operating system is out of memory or resources.
+      errno_code =
+          errno_code > maxErrorCode
               ? NO_ERROR
-              : (return_code == 0 ? ERROR_NOT_ENOUGH_MEMORY : return_code);
+              : (errno_code == 0 ? ERROR_NOT_ENOUGH_MEMORY : errno_code);
 
-      if (return_code != NO_ERROR) {
-        Warning("Can't relaunch %s (0x%.8x).", relaunch_url, return_code);
+      if (errno_code != NO_ERROR) {
+        Warning(
+            "Can't relaunch by %s: %s.", relaunch_url,
+            source::windows::make_windows_errno_info(errno_code).description);
       }
 
-      return_code =
+      errno_code =
           static_cast<u32>(RegDeleteValueW(source_key, L"Relaunch URL"));
 
-      if (return_code != NO_ERROR) {
+      if (errno_code != NO_ERROR) {
         Warning(
             "Can't delete registry key value Software\\Valve\\Source Relaunch "
-            "URL (0x%.8x).",
-            return_code);
+            "URL: %s.",
+            source::windows::make_windows_errno_info(errno_code).description);
       }
     }
 
-    const u32 return_code = static_cast<u32>(RegCloseKey(source_key));
-
-    if (return_code != NO_ERROR) {
-      Warning("Can't close registry key Software\\Valve\\Source (0x%.8x).",
-              return_code);
+    const u32 errno_code = static_cast<u32>(RegCloseKey(source_key));
+    if (errno_code != NO_ERROR) {
+      Warning("Can't close registry key Software\\Valve\\Source: %s.",
+              source::windows::make_windows_errno_info(errno_code).description);
     }
   }
 }
@@ -294,11 +296,11 @@ ICommandLine *const CreateCommandLine() {
 
 source::windows::windows_errno_code SetProcessPriorityInternal(
     _In_z_ const ch *priority_switch, _In_ DWORD priority_class) {
-  const BOOL return_code{SetPriorityClass(GetCurrentProcess(), priority_class)};
-  if (return_code != FALSE) return S_OK;
+  const BOOL errno_code{SetPriorityClass(GetCurrentProcess(), priority_class)};
+  if (errno_code != FALSE) return S_OK;
 
   const auto errno_info{source::windows::windows_errno_info_last_error()};
-  Warning("Can't set process priority by switch %s: %s.", priority_switch,
+  Warning("%s: Can't set process priority: %s.", priority_switch,
           errno_info.description);
 
   return errno_info.code;
@@ -313,8 +315,8 @@ source::windows::windows_errno_code SetProcessPriorityIfNeeded(
 
   if (is_low_priority && is_high_priority) {
     Error(
-        "Can't set process priority to low and high at the same time. Use one "
-        "of %s,%s switches.",
+        "Can't set process priority to low and high at the same time. Please, "
+        "use single of %s/%s.",
         source::tier0::command_line_switches::kCpuPriorityLow,
         source::tier0::command_line_switches::kCpuPriorityHigh);
     return source::windows::win32_to_windows_errno_code(ERROR_BAD_ARGUMENTS);
@@ -350,9 +352,9 @@ void CleanupSettings(_In_ ICommandLine *const command_line) {
   command_line->RemoveParm("+mat_hdr_level");
 }
 
-source::windows::windows_errno_code RunSteamApplication(
-    _In_ ICommandLine *command_line, _In_z_ const ch *base_directory,
-    _In_ const bool is_text_mode) {
+source::windows::windows_errno_code Run(_In_ ICommandLine *command_line,
+                                        _In_z_ const ch *base_directory,
+                                        _In_ const bool is_text_mode) {
   bool need_restart{true};
   FileSystemAccessLogger file_system_access_logger{base_directory,
                                                    command_line};
@@ -361,12 +363,12 @@ source::windows::windows_errno_code RunSteamApplication(
     SourceAppSystemGroup source_app_system_group{
         base_directory, is_text_mode, command_line, file_system_access_logger};
     CSteamApplication steam_app(&source_app_system_group);
-    int return_code{steam_app.Run()};
+    int errno_code{steam_app.Run()};
 
     need_restart =
         steam_app.GetErrorStage() == SourceAppSystemGroup::INITIALIZATION &&
-            return_code == INIT_RESTART ||
-        return_code == RUN_RESTART;
+            errno_code == INIT_RESTART ||
+        errno_code == RUN_RESTART;
 
     bool should_continue_generate_reslists{false};
     if (!need_restart) {
@@ -388,46 +390,67 @@ SOURCE_API_EXPORT source::windows::windows_errno_code LauncherMain(
     _In_ HINSTANCE instance, _In_ int) {
   if (instance == nullptr) return ERROR_INVALID_HANDLE;
 
-  // First, since almost all depends on this.
+  using namespace source::windows;
+
+  // Parse command line early, since used extensively. Should not use SSE and
+  // /or SSE2.
+  ICommandLine *const command_line = CreateCommandLine();
+
+  // CPU should be supported, or we crash later.
+  auto[cpu_info, errno_code] = source::QueryCpuInfo(sizeof(source::CpuInfo));
+  if (failed(errno_code) || !cpu_info.is_info.has_sse ||
+      !cpu_info.is_info.has_sse2) {
+    if (!command_line->CheckParm("-skip_cpu_checks")) {
+      // Debug infrastructure is not ready, so use Windows message box.
+      return NotifyAboutError(
+          "Sorry, query CPU compatibility for the game is failed. Looks like "
+          "your CPU doesn't support CPUID/SSE/SSE2 instructions, which are "
+          "required to run the game.\n\nYou can try launch the game with "
+          "-skip_cpu_checks flag to skip CPU checks, but there is no "
+          "guarantee that it helps. If the game still fails than you should "
+          "upgrade the CPU to run the game :(.",
+          failed(errno_code)
+              ? errno_code
+              : win32_to_windows_errno_code(ERROR_NOT_SUPPORTED));
+    }
+  }
+
   SetAppInstance(instance);
   SpewOutputFunc(LauncherSpewFunc);
-
-  // Parse command line early, since used extensively.
-  ICommandLine *const command_line = CreateCommandLine();
 
   // Dump memory leaks if needed.
   ScopedMemoryLeakDumper scoped_memory_leak_dumper{
       g_pMemAlloc, command_line->FindParm("-leakcheck") > 0};
 
-  using namespace source::windows;
-
   // Run in text mode? (No graphics or sound).
-  auto [is_text_mode, return_code] = InitTextModeIfNeeded(command_line);
-  if (failed(return_code)) {
-    Error("Can't run in text mode (0x%.8x).", return_code);
-    return return_code;
+  bool is_text_mode;
+  std::tie(is_text_mode, errno_code) = InitTextModeIfNeeded(command_line);
+  if (failed(errno_code)) {
+    Error("The game failed to run in text mode: %s.",
+          make_windows_errno_info(errno_code).description);
+    return errno_code;
   }
 
   // Winsock is ready, skip if can't initialize.
   ScopedWinsockInitializer scoped_winsock_initializer{
       WinsockVersion::Version2_2};
-  return_code = scoped_winsock_initializer.error_code();
-  if (failed(return_code)) {
-    Warning("Winsock 2.2 unavailable: %s.",
-            make_windows_errno_info(return_code).description);
+  errno_code = scoped_winsock_initializer.error_code();
+  if (failed(errno_code)) {
+    Warning("Winsock 2.2 unavailable, networking may not run: %s.",
+            make_windows_errno_info(errno_code).description);
   }
 
   VCRHelpers vcr_helpers;
   const ch *base_directory;
 
   // Find directory exe is running from.
-  std::tie(base_directory, return_code) = ComputeBaseDirectory(command_line);
-  if (succeeded(return_code)) {
+  std::tie(base_directory, errno_code) = ComputeBaseDirectory(command_line);
+  if (succeeded(errno_code)) {
     // VCR helpers is ready.
-    std::tie(vcr_helpers, return_code) = BootstrapVCRHelpers(command_line);
+    std::tie(vcr_helpers, errno_code) = BootstrapVCRHelpers(command_line);
   }
 
-  if (succeeded(return_code)) {
+  if (succeeded(errno_code)) {
     // Rehook the command line through VCR mode.
     CommandLine()->CreateCmdLine(VCRHook_GetCommandLine());
 
@@ -435,10 +458,10 @@ SOURCE_API_EXPORT source::windows::windows_errno_code LauncherMain(
     RemoveSpuriousGameParameters(command_line);
 
     // Set process priority if requested.
-    return_code = SetProcessPriorityIfNeeded(command_line);
+    errno_code = SetProcessPriorityIfNeeded(command_line);
   }
 
-  if (succeeded(return_code)) {
+  if (succeeded(errno_code)) {
     // If game is not run from Steam then add -insecure in order to avoid
     // client timeout message.
     if (command_line->FindParm("-steam") == 0) {
@@ -447,17 +470,16 @@ SOURCE_API_EXPORT source::windows::windows_errno_code LauncherMain(
 
     // Figure out the directory the executable is running from and make that
     // be the current working directory.
-    return_code = SetCurrentDirectoryA(base_directory) != FALSE
-                      ? S_OK
-                      : windows_errno_code_last_error();
+    errno_code = SetCurrentDirectoryA(base_directory) != FALSE
+                     ? S_OK
+                     : windows_errno_code_last_error();
   }
 
-  if (succeeded(return_code)) {
-    return_code =
-        RunSteamApplication(command_line, base_directory, is_text_mode);
+  if (succeeded(errno_code)) {
+    errno_code = Run(command_line, base_directory, is_text_mode);
   }
 
-  RelaunchWithNewLanguageViaSteam();
+  RelaunchIfNeeded();
 
-  return return_code;
+  return errno_code;
 }
