2da9b363265c467f8b7a1d32b5740e07f521cb42
Doug Rattman <d.rattman@aol.com>
2018-06-09 17:48:43 +0300

    [steam] Remove unneeded steam code

--- src_main/common/protocol.h
+++ src_main/common/protocol.h
@@ -10,9 +10,9 @@ enum UpdateType {
 
   LeavePVS,  // Entity left pvs
 
-  DeltaEnt,     // There is a delta for this entity.
-  PreserveEnt,  // Entity stays alive but no delta ( could be LOD, or just
-                // unchanged )
+  DeltaEnt,  // There is a delta for this entity.
+  // Entity stays alive but no delta ( could be LOD, or just unchanged)
+  PreserveEnt,
 
   Finished,  // finished parsing entities successfully
   Failed,    // parsing error occured while reading entities
@@ -29,8 +29,8 @@ enum {
 #define INSTANCE_BASELINE_TABLENAME "instancebaseline"
 #define LIGHT_STYLES_TABLENAME "lightstyles"
 #define USER_INFO_TABLENAME "userinfo"
-#define SERVER_STARTUP_DATA_TABLENAME \
-  "server_query_info"  // the name is a remnant...
+// the name is a remnant...
+#define SERVER_STARTUP_DATA_TABLENAME "server_query_info"
 
 //#define CURRENT_PROTOCOL    1
 
@@ -48,19 +48,19 @@ enum {
 #define NUM_BACKUP_COMMAND_BITS 3
 #define MAX_BACKUP_COMMANDS ((1 << NUM_BACKUP_COMMAND_BITS) - 1)
 
-#define PROTOCOL_AUTHCERTIFICATE \
-  0x01  // Connection from client is using a WON authenticated certificate
-#define PROTOCOL_HASHEDCDKEY \
-  0x02  // Connection from client is using hashed CD key because WON comm.
-        // channel was unreachable
+// Connection from client is using a WON authenticated certificate
+#define PROTOCOL_AUTHCERTIFICATE 0x01
+// Connection from client is using hashed CD key because WON comm.
+// channel was unreachable
+#define PROTOCOL_HASHEDCDKEY 0x02
 #define PROTOCOL_STEAM 0x03      // Steam certificates
 #define PROTOCOL_LASTVALID 0x03  // Last valid protocol
 
-#define CONNECTIONLESS_HEADER \
-  0xFFFFFFFF  // all OOB packet start with this sequence
-#define STEAM_KEYSIZE \
-  2048  // max size needed to contain a steam authentication key (both server
-        // and client)
+// all OOB packet start with this sequence
+#define CONNECTIONLESS_HEADER 0xFFFFFFFF
+// max size needed to contain a steam authentication key (both server
+// and client)
+#define STEAM_KEYSIZE 2048
 
 // each channel packet has 1 uint8_t of FLAG bits
 #define PACKET_FLAG_RELIABLE (1 << 0)  // packet contains subchannel stream data
@@ -85,15 +85,13 @@ enum {
 #define net_SetConVar 5    // sends one/multiple convar settings
 #define net_SignonState 6  // signals current signon state
 
-//
 // server to client
-//
 
 #define svc_Print 7       // print text to console
 #define svc_ServerInfo 8  // first message from server about game, map etc
 #define svc_SendTable 9   // sends a sendtable description for a game class
-#define svc_ClassInfo \
-  10  // Info about classes (first uint8_t is a CLASSINFO_ define).
+// Info about classes (first byte is a CLASSINFO_ define).
+#define svc_ClassInfo 10
 #define svc_SetPause 11  // tells client if server paused or unpaused
 
 #define svc_CreateStringTable 12  // inits shared string tables
@@ -108,8 +106,8 @@ enum {
 
 #define svc_SetView 18   // sets entity as point of view
 #define svc_FixAngle 19  // sets/corrects players viewangle
-#define svc_CrosshairAngle \
-  20  // adjusts crosshair in auto aim mode to lock on traget
+// adjusts crosshair in auto aim mode to lock on traget
+#define svc_CrosshairAngle 20
 
 #define svc_BSPDecal 21  // add a static decal to the worl BSP
 // NOTE: This is now unused!
@@ -130,25 +128,22 @@ enum {
 #define svc_Menu 29  // display a menu from a plugin
 
 #define svc_GameEventList 30  // list of known games events and fields
-
-#define svc_GetCvarValue \
-  31  // Server wants to know the value of a cvar on the client.
+// Server wants to know the value of a cvar on the client.
+#define svc_GetCvarValue 31
 
 #define SVC_LASTMSG 31  // last known server messages
 
-//
 // client to server
-//
 
 #define clc_ClientInfo 8     // client info (table CRC etc)
 #define clc_Move 9           // [CUserCmd]
 #define clc_VoiceData 10     // Voicestream data from a client
 #define clc_BaselineAck 11   // client acknowledges a new baseline seqnr
 #define clc_ListenEvents 12  // client acknowledges a new baseline seqnr
-#define clc_RespondCvarValue \
-  13  // client is responding to a svc_GetCvarValue message.
-#define clc_FileCRCCheck \
-  14  // client is sending a file's CRC to the server to be verified.
+// client is responding to a svc_GetCvarValue message.
+#define clc_RespondCvarValue 13
+// client is sending a file's CRC to the server to be verified.
+#define clc_FileCRCCheck 14
 
 #define CLC_LASTMSG 14  //	last known client message
 
@@ -157,18 +152,16 @@ enum {
 
 #define SIGNONSTATE_NONE 0       // no state yet, about to connect
 #define SIGNONSTATE_CHALLENGE 1  // client challenging server, all OOB packets
-#define SIGNONSTATE_CONNECTED \
-  2                             // client is connected to server, netchans ready
+// client is connected to server, netchans ready
+#define SIGNONSTATE_CONNECTED 2
 #define SIGNONSTATE_NEW 3       // just got serverinfo and string tables
 #define SIGNONSTATE_PRESPAWN 4  // received signon buffers
 #define SIGNONSTATE_SPAWN 5     // ready to receive entity packets
-#define SIGNONSTATE_FULL \
-  6  // we are fully connected, first non-delta packet received
+// we are fully connected, first non-delta packet received
+#define SIGNONSTATE_FULL 6
 #define SIGNONSTATE_CHANGELEVEL 7  // server is changing level, please wait
 
-//
 // matchmaking
-//
 
 #define mm_Heartbeat 16         // send a mm_Heartbeat
 #define mm_ClientInfo 17        // information about a player
